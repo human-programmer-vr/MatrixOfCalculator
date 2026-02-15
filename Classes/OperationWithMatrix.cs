e10c5812327fc549d9830a190456256a8702a2cd
human-programmer-vr <dima.ostryakov@yandex.ru>
2026-02-15 15:39:32 +0300

    Update OperationWithMatrix.cs

--- Classes/OperationWithMatrix.cs
+++ Classes/OperationWithMatrix.cs
@@ -5,7 +5,7 @@ namespace MatrixOfCalculator.Classes
 {
     public class OperationWithMatrix
     {
-        private static double[,] _temp;
+        private static short[,] _temp;
 
         /// <summary>
         /// Заполнение данных матрицы через ручной ввод данных
@@ -13,10 +13,10 @@ namespace MatrixOfCalculator.Classes
         /// <param name="matrix"></param>
         /// <param name="input"></param>
         /// <returns></returns>
-        public static double[,] HandleInput(double[,] matrix, params double[] input)
+        public static short[,] HandleInput(short[,] matrix, params short[] input)
         {
-            for (int x = 0, index = 0; x < matrix.GetLength(0); x++)
-                for (int y = 0; y < matrix.GetLength(1); y++, index++)
+            for (short x = 0, index = 0; x < matrix.GetLength(0); x++)
+                for (short y = 0; y < matrix.GetLength(1); y++, index++)
                     matrix[x, y] = input[index];
 
             return matrix;
@@ -28,12 +28,12 @@ namespace MatrixOfCalculator.Classes
         /// <param name="matrix"></param>
         /// <param name="multiplicate"></param>
         /// <returns></returns>
-        public static double[,] MultiplicateNumberOnMatrix(double[,] matrix, int multiplicate)
+        public static short[,] MultiplicateNumberOnMatrix(short[,] matrix, byte multiplicate)
         {
-            _temp = new double[matrix.GetLength(0), matrix.GetLength(1)];
+            _temp = new short[matrix.GetLength(0), matrix.GetLength(1)];
 
-            for (int x = 0; x < matrix.GetLength(0); x++)
-                for (int y = 0; y < matrix.GetLength(1); y++)
+            for (short x = 0; x < matrix.GetLength(0); x++)
+                for (short y = 0; y < matrix.GetLength(1); y++)
                     _temp[x,y] = matrix[x, y] * multiplicate;
 
             return _temp;
@@ -44,7 +44,7 @@ namespace MatrixOfCalculator.Classes
         /// </summary>
         /// <param name="matrix"></param>
         /// <returns></returns>
-        private static double FindDeterminantTwoOnTwo(double[,] matrix)
+        private static short FindDeterminantTwoOnTwo(short[,] matrix)
         {
             return (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
         }
@@ -54,7 +54,7 @@ namespace MatrixOfCalculator.Classes
         /// </summary>
         /// <param name="matrix"></param>
         /// <returns></returns>
-        private static double FindDeterminantThreeOnThree(double[,] matrix)
+        private static short FindDeterminantThreeOnThree(short[,] matrix)
         {
             return 
                 matrix[0, 0] * matrix[1, 1] * matrix[2, 2] +
@@ -77,7 +77,7 @@ namespace MatrixOfCalculator.Classes
             if (matrix.Length != 4 && matrix.Length != 9)
                 return matrix;
 
-            double determinant = 0;
+            short determinant = 0;
 
             if (matrix.Length == 4)
             {
@@ -114,13 +114,13 @@ namespace MatrixOfCalculator.Classes
                     fifthumber, sixthNumber, seventhNumber, eighthNumber, ninethNumber);
             }
 
-            for (int x = 0; x < _temp.GetLength(0); x++)
-                for (int y = 0; y < _temp.GetLength(1); y++)
+            for (short x = 0; x < _temp.GetLength(0); x++)
+                for (short y = 0; y < _temp.GetLength(1); y++)
                     if((x + y) % 2 != 0)
                         _temp[x, y] *= -1; 
 
-            for (int x = 0; x < _temp.GetLength(0); x++)
-                for (int y = 0; y < _temp.GetLength(1); y++)
+            for (short x = 0; x < _temp.GetLength(0); x++)
+                for (short y = 0; y < _temp.GetLength(1); y++)
                     _temp[x, y] /= determinant; 
 
             return _temp;
@@ -133,10 +133,10 @@ namespace MatrixOfCalculator.Classes
         /// <returns></returns>
         public static double[,] TransposeMatrix(double[,] matrix)
         {
-            _temp = new double[matrix.GetLength(0), matrix.GetLength(1)];
+            _temp = new short[matrix.GetLength(0), matrix.GetLength(1)];
 
-            for (int x = 0; x < matrix.GetLength(0); x++)
-                for (int y = 0; y < matrix.GetLength(1); y++)
+            for (short x = 0; x < matrix.GetLength(0); x++)
+                for (short y = 0; y < matrix.GetLength(1); y++)
                     _temp[y, x] = matrix[x, y];
 
             return _temp;
@@ -148,12 +148,12 @@ namespace MatrixOfCalculator.Classes
         /// <param name="matrixOne"></param>
         /// <param name="matrixTwo"></param>
         /// <returns></returns>
-        public static double[,] AdditionMatrix(double[,] matrixOne, double[,] matrixTwo)
+        public static short[,] AdditionMatrix(short[,] matrixOne, short[,] matrixTwo)
         {
-            _temp = new double[matrixOne.GetLength(0), matrixOne.GetLength(1)];
+            _temp = new short[matrixOne.GetLength(0), matrixOne.GetLength(1)];
 
-            for (int x = 0; x < matrixOne.GetLength(0); x++)
-                for (int y = 0; y < matrixOne.GetLength(1); y++)
+            for (short x = 0; x < matrixOne.GetLength(0); x++)
+                for (short y = 0; y < matrixOne.GetLength(1); y++)
                     _temp[x, y] = matrixOne[x, y] + matrixTwo[x, y];
 
             return _temp;
@@ -165,12 +165,12 @@ namespace MatrixOfCalculator.Classes
         /// <param name="matrixOne"></param>
         /// <param name="matrixTwo"></param>
         /// <returns></returns>
-        public static double[,] SubstractionMatrix(double[,] matrixOne, double[,] matrixTwo)
+        public static short[,] SubstractionMatrix(short[,] matrixOne, short[,] matrixTwo)
         {
-            _temp = new double[matrixOne.GetLength(0), matrixOne.GetLength(1)];
+            _temp = new short[matrixOne.GetLength(0), matrixOne.GetLength(1)];
 
-            for (int x = 0; x < matrixTwo.GetLength(0); x++)
-                for (int y = 0; y < matrixTwo.GetLength(1); y++)
+            for (short x = 0; x < matrixTwo.GetLength(0); x++)
+                for (short y = 0; y < matrixTwo.GetLength(1); y++)
                     _temp[x, y] = matrixTwo[x, y] - matrixOne[x, y];
 
             return _temp;
@@ -182,58 +182,58 @@ namespace MatrixOfCalculator.Classes
         /// <param name="matrixOne"></param>
         /// <param name="matrixTwo"></param>
         /// <returns></returns>
-        public static double[,] MultiplicateMatrix(double[,] matrixOne, double[,] matrixTwo)
+        public static short[,] MultiplicateMatrix(short[,] matrixOne, short[,] matrixTwo)
         {
-            _temp = new double[matrixOne.GetLength(0), matrixOne.GetLength(1)];
+            _temp = new short[matrixOne.GetLength(0), matrixOne.GetLength(1)];
 
             if (matrixOne.Length == 4)
             {
-                double firstNumber = matrixOne[0, 0] * matrixTwo[0, 0] + matrixOne[0, 1] * matrixTwo[1, 0];
-                double secondNumber = matrixOne[0, 0] * matrixTwo[0, 1] + matrixOne[0, 1] * matrixTwo[1, 1];
-                double thirdNumber = matrixOne[1, 0] * matrixTwo[0, 0] + matrixOne[1, 1] * matrixTwo[1, 0];
-                double fourthNumber = matrixOne[1, 0] * matrixTwo[0, 1] + matrixOne[1, 1] * matrixTwo[1, 1];
+                short firstNumber = matrixOne[0, 0] * matrixTwo[0, 0] + matrixOne[0, 1] * matrixTwo[1, 0];
+                short secondNumber = matrixOne[0, 0] * matrixTwo[0, 1] + matrixOne[0, 1] * matrixTwo[1, 1];
+                short thirdNumber = matrixOne[1, 0] * matrixTwo[0, 0] + matrixOne[1, 1] * matrixTwo[1, 0];
+                short fourthNumber = matrixOne[1, 0] * matrixTwo[0, 1] + matrixOne[1, 1] * matrixTwo[1, 1];
 
                 return HandleInput(_temp, firstNumber, secondNumber, thirdNumber, fourthNumber);
             }
 
             if (matrixOne.Length == 9)
             {
-                double firstNumber = matrixOne[0, 0] * matrixTwo[0, 0] + matrixOne[0, 1] * matrixTwo[1, 0] + matrixOne[0, 2] * matrixTwo[2, 0];
-                double secondNumber = matrixOne[0, 0] * matrixTwo[0, 1] + matrixOne[0, 1] * matrixTwo[1, 1] + matrixOne[0, 2] * matrixTwo[2, 1];
-                double thirdNumber = matrixOne[0, 0] * matrixTwo[0, 2] + matrixOne[0, 1] * matrixTwo[1, 2] + matrixOne[0, 2] * matrixTwo[2, 2];
+                short firstNumber = matrixOne[0, 0] * matrixTwo[0, 0] + matrixOne[0, 1] * matrixTwo[1, 0] + matrixOne[0, 2] * matrixTwo[2, 0];
+                short secondNumber = matrixOne[0, 0] * matrixTwo[0, 1] + matrixOne[0, 1] * matrixTwo[1, 1] + matrixOne[0, 2] * matrixTwo[2, 1];
+                short thirdNumber = matrixOne[0, 0] * matrixTwo[0, 2] + matrixOne[0, 1] * matrixTwo[1, 2] + matrixOne[0, 2] * matrixTwo[2, 2];
 
-                double fourthNumber = matrixOne[1, 0] * matrixTwo[0, 0] + matrixOne[1, 1] * matrixTwo[1, 0] + matrixOne[1, 2] * matrixTwo[2, 0];
-                double fifthNumber = matrixOne[1, 0] * matrixTwo[0, 1] + matrixOne[1, 1] * matrixTwo[1, 1] + matrixOne[1, 2] * matrixTwo[2, 1];
-                double sixteenNumber = matrixOne[1, 0] * matrixTwo[0, 2] + matrixOne[1, 1] * matrixTwo[1, 2] + matrixOne[1, 2] * matrixTwo[2, 2];
+                short fourthNumber = matrixOne[1, 0] * matrixTwo[0, 0] + matrixOne[1, 1] * matrixTwo[1, 0] + matrixOne[1, 2] * matrixTwo[2, 0];
+                short fifthNumber = matrixOne[1, 0] * matrixTwo[0, 1] + matrixOne[1, 1] * matrixTwo[1, 1] + matrixOne[1, 2] * matrixTwo[2, 1];
+                short sixteenNumber = matrixOne[1, 0] * matrixTwo[0, 2] + matrixOne[1, 1] * matrixTwo[1, 2] + matrixOne[1, 2] * matrixTwo[2, 2];
 
-                double seventeenNumber = matrixOne[2, 0] * matrixTwo[0, 0] + matrixOne[2, 1] * matrixTwo[1, 0] + matrixOne[2, 2] * matrixTwo[2, 0];
-                double eigteenNumber = matrixOne[2, 0] * matrixTwo[0, 1] + matrixOne[2, 1] * matrixTwo[1, 1] + matrixOne[2, 2] * matrixTwo[2, 1];
-                double nineteenNumber = matrixOne[2, 0] * matrixTwo[0, 2] + matrixOne[2, 1] * matrixTwo[1, 2] + matrixOne[2, 2]* matrixTwo[2, 2];
+                short seventeenNumber = matrixOne[2, 0] * matrixTwo[0, 0] + matrixOne[2, 1] * matrixTwo[1, 0] + matrixOne[2, 2] * matrixTwo[2, 0];
+                short eigteenNumber = matrixOne[2, 0] * matrixTwo[0, 1] + matrixOne[2, 1] * matrixTwo[1, 1] + matrixOne[2, 2] * matrixTwo[2, 1];
+                short nineteenNumber = matrixOne[2, 0] * matrixTwo[0, 2] + matrixOne[2, 1] * matrixTwo[1, 2] + matrixOne[2, 2]* matrixTwo[2, 2];
 
                 return HandleInput(_temp, firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixteenNumber, seventeenNumber, eigteenNumber, nineteenNumber);
             }
 
             if (matrixOne.Length == 16) 
             {
-                double firstNumber = matrixOne[0, 0] * matrixTwo[0, 0] + matrixOne[0, 1] * matrixTwo[1, 0] + matrixOne[0, 2] * matrixTwo[2, 0] + matrixOne[0, 3] * matrixTwo[3, 0];
-                double secondNumber = matrixOne[0, 0] * matrixTwo[0, 1] + matrixOne[0, 1] * matrixTwo[1, 1] + matrixOne[0, 2] * matrixTwo[2, 1] + matrixOne[0, 3] * matrixTwo[3, 1];
-                double thirdNumber = matrixOne[0, 0] * matrixTwo[0, 2] + matrixOne[0, 1] * matrixTwo[1, 2] + matrixOne[0, 2] * matrixTwo[2, 2] + matrixOne[0, 3] * matrixTwo[3, 2];
-                double fourthNumber = matrixOne[0, 0] * matrixTwo[0, 3] + matrixOne[0, 1] * matrixTwo[1, 3] + matrixOne[0, 2] * matrixTwo[2, 3] + matrixOne[0, 3] * matrixTwo[3, 3];
+                short firstNumber = matrixOne[0, 0] * matrixTwo[0, 0] + matrixOne[0, 1] * matrixTwo[1, 0] + matrixOne[0, 2] * matrixTwo[2, 0] + matrixOne[0, 3] * matrixTwo[3, 0];
+                short secondNumber = matrixOne[0, 0] * matrixTwo[0, 1] + matrixOne[0, 1] * matrixTwo[1, 1] + matrixOne[0, 2] * matrixTwo[2, 1] + matrixOne[0, 3] * matrixTwo[3, 1];
+                short thirdNumber = matrixOne[0, 0] * matrixTwo[0, 2] + matrixOne[0, 1] * matrixTwo[1, 2] + matrixOne[0, 2] * matrixTwo[2, 2] + matrixOne[0, 3] * matrixTwo[3, 2];
+                short fourthNumber = matrixOne[0, 0] * matrixTwo[0, 3] + matrixOne[0, 1] * matrixTwo[1, 3] + matrixOne[0, 2] * matrixTwo[2, 3] + matrixOne[0, 3] * matrixTwo[3, 3];
 
-                double fifthNumber = matrixOne[1, 0] * matrixTwo[0, 0] + matrixOne[1, 1] * matrixTwo[1, 0] + matrixOne[1, 2] * matrixTwo[2, 0] + matrixOne[1, 3] * matrixTwo[3, 0];             
-                double sixthNumber = matrixOne[1, 0] * matrixTwo[0, 1] + matrixOne[1, 1] * matrixTwo[1, 1] + matrixOne[1, 2] * matrixTwo[2, 1] + matrixOne[1, 3] * matrixTwo[3, 1];                
-                double seventeenNumber = matrixOne[1, 0] * matrixTwo[0, 2] + matrixOne[1, 1] * matrixTwo[1, 2] + matrixOne[1, 2] * matrixTwo[2, 2] + matrixOne[1, 3] * matrixTwo[3, 2];
-                double eigteenNumber = matrixOne[1, 0] * matrixTwo[0, 3] + matrixOne[1, 1] * matrixTwo[1, 3] + matrixOne[1, 2] * matrixTwo[2, 3] + matrixOne[1, 3] * matrixTwo[3, 3];
+                short fifthNumber = matrixOne[1, 0] * matrixTwo[0, 0] + matrixOne[1, 1] * matrixTwo[1, 0] + matrixOne[1, 2] * matrixTwo[2, 0] + matrixOne[1, 3] * matrixTwo[3, 0];             
+                short sixthNumber = matrixOne[1, 0] * matrixTwo[0, 1] + matrixOne[1, 1] * matrixTwo[1, 1] + matrixOne[1, 2] * matrixTwo[2, 1] + matrixOne[1, 3] * matrixTwo[3, 1];                
+                short seventeenNumber = matrixOne[1, 0] * matrixTwo[0, 2] + matrixOne[1, 1] * matrixTwo[1, 2] + matrixOne[1, 2] * matrixTwo[2, 2] + matrixOne[1, 3] * matrixTwo[3, 2];
+                short eigteenNumber = matrixOne[1, 0] * matrixTwo[0, 3] + matrixOne[1, 1] * matrixTwo[1, 3] + matrixOne[1, 2] * matrixTwo[2, 3] + matrixOne[1, 3] * matrixTwo[3, 3];
 
-                double nineteenNumber = matrixOne[2, 0] * matrixTwo[0, 0] + matrixOne[2, 1] * matrixTwo[1, 0] + matrixOne[2, 2] * matrixTwo[2, 0] + matrixOne[2, 3] * matrixTwo[3, 0];
-                double tenthNumber = matrixOne[2, 0] * matrixTwo[0, 1] + matrixOne[2, 1] * matrixTwo[1, 1] + matrixOne[2, 2] * matrixTwo[2, 1] + matrixOne[2, 3] * matrixTwo[3, 1];
-                double eleventhNumber = matrixOne[2, 0] * matrixTwo[0, 2] + matrixOne[2, 1] * matrixTwo[1, 2] + matrixOne[2, 2] * matrixTwo[2, 2] + matrixOne[2, 3] * matrixTwo[3, 2];
-                double twelveNumber = matrixOne[2, 0] * matrixTwo[0, 3] + matrixOne[2, 1] * matrixTwo[1, 3] + matrixOne[2, 2] * matrixTwo[2, 3] + matrixOne[2, 3] * matrixTwo[3, 3];
+                short nineteenNumber = matrixOne[2, 0] * matrixTwo[0, 0] + matrixOne[2, 1] * matrixTwo[1, 0] + matrixOne[2, 2] * matrixTwo[2, 0] + matrixOne[2, 3] * matrixTwo[3, 0];
+                short tenthNumber = matrixOne[2, 0] * matrixTwo[0, 1] + matrixOne[2, 1] * matrixTwo[1, 1] + matrixOne[2, 2] * matrixTwo[2, 1] + matrixOne[2, 3] * matrixTwo[3, 1];
+                short eleventhNumber = matrixOne[2, 0] * matrixTwo[0, 2] + matrixOne[2, 1] * matrixTwo[1, 2] + matrixOne[2, 2] * matrixTwo[2, 2] + matrixOne[2, 3] * matrixTwo[3, 2];
+                short twelveNumber = matrixOne[2, 0] * matrixTwo[0, 3] + matrixOne[2, 1] * matrixTwo[1, 3] + matrixOne[2, 2] * matrixTwo[2, 3] + matrixOne[2, 3] * matrixTwo[3, 3];
 
-                double thirteenNumber = matrixOne[3, 0] * matrixTwo[0, 0] + matrixOne[3, 1] * matrixTwo[1, 0] + matrixOne[3, 2] * matrixTwo[2, 0] + matrixOne[3, 3] * matrixTwo[3, 0];
-                double fourteenNumber = matrixOne[3, 0] * matrixTwo[0, 1] + matrixOne[3, 1] * matrixTwo[1, 1] + matrixOne[3, 2] * matrixTwo[2, 1] + matrixOne[3, 3] * matrixTwo[3, 1];
-                double fifteenNumber = matrixOne[3, 0] * matrixTwo[0, 2] + matrixOne[3, 1] * matrixTwo[1, 2] + matrixOne[3, 2] * matrixTwo[2, 2] + matrixOne[3, 3] * matrixTwo[3, 2];
-                double sixteenNumber = matrixOne[3, 0] * matrixTwo[0, 3] + matrixOne[3, 1] * matrixTwo[1, 3] + matrixOne[3, 2] * matrixTwo[2, 3] + matrixOne[3, 3] * matrixTwo[3, 3];
+                short thirteenNumber = matrixOne[3, 0] * matrixTwo[0, 0] + matrixOne[3, 1] * matrixTwo[1, 0] + matrixOne[3, 2] * matrixTwo[2, 0] + matrixOne[3, 3] * matrixTwo[3, 0];
+                short fourteenNumber = matrixOne[3, 0] * matrixTwo[0, 1] + matrixOne[3, 1] * matrixTwo[1, 1] + matrixOne[3, 2] * matrixTwo[2, 1] + matrixOne[3, 3] * matrixTwo[3, 1];
+                short fifteenNumber = matrixOne[3, 0] * matrixTwo[0, 2] + matrixOne[3, 1] * matrixTwo[1, 2] + matrixOne[3, 2] * matrixTwo[2, 2] + matrixOne[3, 3] * matrixTwo[3, 2];
+                short sixteenNumber = matrixOne[3, 0] * matrixTwo[0, 3] + matrixOne[3, 1] * matrixTwo[1, 3] + matrixOne[3, 2] * matrixTwo[2, 3] + matrixOne[3, 3] * matrixTwo[3, 3];
 
                 return HandleInput(_temp, firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber, seventeenNumber, eigteenNumber, 
                     nineteenNumber, tenthNumber, eleventhNumber, twelveNumber, thirteenNumber, fourteenNumber, fifteenNumber, sixteenNumber);
@@ -242,4 +242,5 @@ namespace MatrixOfCalculator.Classes
             return null;
         }
     }
+
 }
