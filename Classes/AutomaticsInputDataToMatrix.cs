c474a4f8c21488ca281ef866a926733bb424b2f9
human-programmer-vr <dima.ostryakov@yandex.ru>
2026-02-15 15:32:43 +0300

    Update AutomaticsInputDataToMatrix.cs

--- Classes/AutomaticsInputDataToMatrix.cs
+++ Classes/AutomaticsInputDataToMatrix.cs
@@ -11,13 +11,14 @@ namespace MatrixOfCalculator.Classes
         /// </summary>
         /// <param name="matrix"></param>
         /// <returns></returns>
-        public static double[,] AutoInput(double[,] matrix)
+        public static short[,] AutoInput(short[,] matrix)
         {
-            for (int x = 0; x < matrix.GetLength(0); x++)
-                for (int y = 0; y < matrix.GetLength(1); y++)
+            for (short x = 0; x < matrix.GetLength(0); x++)
+                for (short y = 0; y < matrix.GetLength(1); y++)
                     matrix[x, y] = _rand.Next(-30, 30);
 
             return matrix;
         }
     }
+
 }
